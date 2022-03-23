6244c6f6d79250ca750a8cb8528382df16e60ffb
sdziuda <szymondziuda95@gmail.com>
2022-03-23 15:24:55 +0100

    Added printing "Hello world"

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,6 +3,7 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Hello world");
     final var number = new BigInt("42");
     System.out.println(number);
   }
