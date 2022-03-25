fbdc1beb42436e6ad9cc583f254487acb1162e7a
sdziuda <szymondziuda95@gmail.com>
2022-03-25 13:28:51 +0100

    Changed number back
    Changed number back to 42

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,7 +3,7 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
-    final var number = new BigInt("00042");
+    final var number = new BigInt("42");
     System.out.println(number);
   }
 }
