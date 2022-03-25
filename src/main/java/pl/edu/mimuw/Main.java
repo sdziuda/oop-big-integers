caf15e5f9ef6bd6c302a5972096079c189fdf6d7
sdziuda <szymondziuda95@gmail.com>
2022-03-25 17:13:36 +0100

    Changed number
    Changed number to 420 to test automatic tests on github

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,7 +3,7 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
-    final var number = new BigInt("42");
+    final var number = new BigInt("420");
     System.out.println(number);
   }
 }
